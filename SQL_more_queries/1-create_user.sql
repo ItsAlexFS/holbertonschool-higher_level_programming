9bacfe1074b8cca63d3d915f696a00ec521ad949
ItsAlexFS <alexfitzsolis123@gmail.com>
2026-02-23 02:33:02 -0600

    fixed stuff

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- user
 DROP USER IF EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
 
